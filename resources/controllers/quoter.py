9946a54a8930f67b84cfa1da815af38ddbbb4461
Javier Carrrasco Otarola <javiercarrasco@MacBook-Pro-de-Javier.local>
2024-10-03 09:30:18 -0300

    moment update now doesnt reset form when succesful

--- resources/controllers/quoter.py
+++ resources/controllers/quoter.py
@@ -258,7 +258,7 @@ class QuoterInitApi(Resource):
 
       data={}
       data["hectares"]=total_hectares
-      data["usd2clp"]=896.25
+      data["usd2clp"]=908.23
       data["clp2usd"]=0.0011
       data["products"]=final_list
 
@@ -453,7 +453,7 @@ class QuoterApi(Resource):
       
       data={}
      
-      data["usd2clp"]=896.25
+      data["usd2clp"]=908.23
       data["clp2usd"]=0.0011
 
       
