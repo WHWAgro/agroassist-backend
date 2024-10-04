89bb7af564f1a586c8f7349bb9df5f6304084b1f
Javier Carrrasco Otarola <javiercarrasco@MacBook-Pro-de-Javier.local>
2024-10-04 11:14:16 -0300

    moment update now doesnt reset form when succesful

--- resources/controllers/user.py
+++ resources/controllers/user.py
@@ -185,7 +185,7 @@ class LoginUserApi(Resource):
             data["machinery_type"]=[{'_id':1,'machinery_type_name':'Tractor'},{'_id':2,'machinery_type_name':'Nebulizador'},{'_id':3,'machinery_type_name':'Bomba de espalda'}]
             data["task_type"]=[{'_id':1,'task_type_name':'Fitosanitario'},{'_id':2,'task_type_name':'Otras'},{'_id':3,'task_type_name':'Riego'},{'_id':4,'task_type_name':'Cosecha'}]
             
-            data["event_type"]=[{'_id':1,'event_type_name':'Lluvia'},{'_id':2,'event_type_name':'Plaga'},{'_id':3,'event_type_name':'Helada'}]
+            data["event_type"]=[{'_id':1,'event_type_name':'Lluvia'},{'_id':2,'event_type_name':'Plaga'},{'_id':3,'event_type_name':'Helada'},{'_id':4,'event_type_name':'Poda'},{'_id':5,'event_type_name':'Cosecha'}]
             #for product in data['products']:
               #if product['dosage_type']==1:
 
