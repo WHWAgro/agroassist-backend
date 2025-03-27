dda5de27de013b2922ebebaf45534051928098b6
Javier Carrrasco Otarola <jcarrascootarola@gmail.com>
2025-03-27 08:40:28 -0300

    trigger avent now only on selected fields

--- resources/controllers/user.py
+++ resources/controllers/user.py
@@ -322,7 +322,7 @@ class LoginUserApi(Resource):
             data["dosage_parts_per_unit"]={ "1":{"1":"gr/100L","2":"Kg/100L","3":"g/Ha","4":"Kg/Ha","9":"gr Ingrediente Activo/100L","10":"Kg Ingrediente Activo/100L"}, "2":{"5":"cc/100L","6":"L/100L","7":"cc/Ha","8":"L/Ha","11":"cc Ingrediente Activo/100L","12":"L Ingrediente Activo/100L"}}
             data["task_status"]=[{'_id':1,'status_name':'Pendiente'},{'_id':3,'status_name':'Orden Generada'},{'_id':4,'status_name':'En Proceso'},{'_id':5,'status_name':'Omitida'},{'_id':2,'status_name':'Finalizada'}]
             data["worker_type"]=[{'_id':1,'worker_type_name':'Administrador'},{'_id':2,'worker_type_name':'Asesor Agrícola'},{'_id':3,'worker_type_name':'En Terreno'}]
-            data["machinery_type"]=[{'_id':0,'machinery_type_name':'Otro'},{'_id':1,'machinery_type_name':'Tractor'},{'_id':2,'machinery_type_name':'Nebulizador'},{'_id':3,'machinery_type_name':'Bomba de espalda'}]
+            data["machinery_type"]=[{'_id':4,'machinery_type_name':'Otro'},{'_id':1,'machinery_type_name':'Tractor'},{'_id':2,'machinery_type_name':'Nebulizador'},{'_id':3,'machinery_type_name':'Bomba de espalda'}]
             data["task_type"]=[{'_id':1,'task_type_name':'Aplicación'},{'_id':2,'task_type_name':'Otras'},{'_id':3,'task_type_name':'Riego'},{'_id':4,'task_type_name':'Cosecha'}]
             
             data["event_type"]=[{'_id':1,'event_type_name':'Lluvia'},{'_id':2,'event_type_name':'Plaga'},{'_id':3,'event_type_name':'Helada'},{'_id':4,'event_type_name':'Poda'},{'_id':5,'event_type_name':'Cosecha'}]
